7beddd46cf6993214ecfcea8d440aa738039b6e3
amac81 <arnaldocanelas@gmail.com>
2024-07-03 16:59:56 +0100

    application properties created; application-test.properties created; h2 database configure

--- src/main/java/bt/bitclinic/java_accommerce/entities/User.java
+++ src/main/java/bt/bitclinic/java_accommerce/entities/User.java
@@ -2,7 +2,17 @@ package bt.bitclinic.java_accommerce.entities;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
