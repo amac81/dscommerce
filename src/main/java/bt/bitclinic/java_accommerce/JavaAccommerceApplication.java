bfc59a32cbdf91185fa5a48b8cc2981d864347a0
amac81 <arnaldocanelas@gmail.com>
2024-07-03 16:22:57 +0100

    project created

---
+++ src/main/java/bt/bitclinic/java_accommerce/JavaAccommerceApplication.java
@@ -0,0 +1,13 @@
+package bt.bitclinic.java_accommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAccommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAccommerceApplication.class, args);
+	}
+
+}
