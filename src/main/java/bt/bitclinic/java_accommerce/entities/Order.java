5b0ba5458ba90e4bd200961d410cdc10a326409e
amac81 <arnaldocanelas@gmail.com>
2024-07-04 12:47:08 +0100

    Class Category created; Class Product created

--- src/main/java/bt/bitclinic/java_accommerce/entities/Order.java
+++ src/main/java/bt/bitclinic/java_accommerce/entities/Order.java
@@ -34,14 +34,13 @@ public class Order {
 	
 	public Order() {}
 
-	public Order(Long id, Instant moment, OrderStatus status, User client) {
+	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
 		this.id = id;
 		this.moment = moment;
 		this.status = status;
 		this.client = client;
+		this.payment = payment;
 	}
-
-
 
 	public Long getId() {
 		return id;
@@ -75,6 +74,12 @@ public class Order {
 		this.client = client;
 	}
 
-	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 }
