08af66c75d8c7673abd002b952e7ed4a62264232
amac81 <arnaldocanelas@gmail.com>
2024-07-04 14:08:02 +0100

    Class Category update; Class Product update

--- src/main/java/bt/bitclinic/java_accommerce/entities/Product.java
+++ src/main/java/bt/bitclinic/java_accommerce/entities/Product.java
@@ -1,9 +1,15 @@
 package bt.bitclinic.java_accommerce.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +23,12 @@ public class Product {
 	private String description;
 	private Double price;
 	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {}
 
@@ -67,9 +79,9 @@ public class Product {
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
-	
 
-	
-	
-	
+	public Set<Category> getCategories() {
+		return categories;
+	}
+		
 }
