05cee0ad0c1fd994c755734fd7279a1ef9e9f220
amac81 <arnaldocanelas@gmail.com>
2024-07-04 16:23:31 +0100

    Equals and Hascode methods created

--- src/main/java/bt/bitclinic/java_accommerce/entities/Category.java
+++ src/main/java/bt/bitclinic/java_accommerce/entities/Category.java
@@ -1,6 +1,7 @@
 package bt.bitclinic.java_accommerce.entities;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
@@ -49,4 +50,22 @@ public class Category {
 		return products;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
 }
