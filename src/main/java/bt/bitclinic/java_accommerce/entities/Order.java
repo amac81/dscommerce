e35057a0885583324962d29013cbf388d101d884
amac81 <arnaldocanelas@gmail.com>
2024-07-04 10:13:58 +0100

    OrderStatus enum createad; Class Order created

---
+++ src/main/java/bt/bitclinic/java_accommerce/entities/Order.java
@@ -0,0 +1,58 @@
+package bt.bitclinic.java_accommerce.entities;
+
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_order")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant moment;
+	private OrderStatus status;
+	
+	public Order() {}
+	
+	public Order(Long id, Instant moment, OrderStatus status) {
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", moment=" + moment + ", status=" + status + "]";
+	}	
+
+}
---
+++ src/main/java/bt/bitclinic/java_accommerce/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package bt.bitclinic.java_accommerce.entities;
+
+public enum OrderStatus {
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELED;
+}
