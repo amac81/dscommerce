89280fccb76dc2b057704de9cbb39989bdf52012
amac81 <arnaldocanelas@gmail.com>
2024-10-15 17:05:49 +0100

    UserService update

--- src/main/java/pt/bitclinic/javaaccommerce/services/UserService.java
+++ src/main/java/pt/bitclinic/javaaccommerce/services/UserService.java
@@ -1,17 +1,30 @@
 package pt.bitclinic.javaaccommerce.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import pt.bitclinic.javaaccommerce.projections.UserDetailsProjection;
+import pt.bitclinic.javaaccommerce.repositories.UserRepository;
+
 @Service
 public class UserService implements UserDetailsService{
 
+	@Autowired
+	UserRepository userRepository;
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
+		
+		if(result.size()==0)
+			return null;
+		else
+			
 	}
 	
 }
