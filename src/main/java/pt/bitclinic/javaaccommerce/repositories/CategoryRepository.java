bd7f8edb02b054c8ccf18175e24ff041606d6788
amac81 <arnaldocanelas@gmail.com>
2024-10-22 15:59:38 +0100

    get Categories

--- src/main/java/pt/bitclinic/javaaccommerce/repositories/CategoryRepository.java
+++ src/main/java/pt/bitclinic/javaaccommerce/repositories/CategoryRepository.java
@@ -1,10 +1,16 @@
 package pt.bitclinic.javaaccommerce.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import pt.bitclinic.javaaccommerce.entities.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long>
 {
-
+	@Query("SELECT obj FROM Category obj WHERE "
+			+ "UPPER(obj.name) LIKE "
+			+ "UPPER(CONCAT('%', :name, '%'))")
+	Page<Category> searchByName(String name, Pageable pageable);
 }
